2649e5cfd1bc343580734c029611253c9aa24aab
Abdullah Ali <abdullaham1422@gmail.com>
2023-12-02 16:06:10 +0300

    Allow to write 10 number in ID and Phone

--- src/components/EditProfile.tsx
+++ src/components/EditProfile.tsx
@@ -48,6 +48,14 @@ export default function EditProfile() {
     'Other' // When 'Other' is selected, a text box will appear for the user to specify
   ];
 
+  const handleNumericInputChange = (setterFunction, maxLength) => (event) => {
+    // Check if the input value is numeric and set the state
+    const value = event.target.value;
+    if ((value === '' || /^[0-9\b]+$/.test(value)) && value.length <= maxLength) {
+        setterFunction(value);
+      }
+  };
+
   return (
     <div className="bg-[#f7f7f7] pt-16 flex flex-col items-center min-h-screen font-roboto">
       <div className="w-full max-w-4xl bg-white rounded-lg shadow-lg overflow-hidden flex flex-col items-center p-8 space-y-8">
@@ -60,17 +68,17 @@ export default function EditProfile() {
           <form className="space-y-4">
             {/* User ID */}
             <div>
-              <label className="block text-sm font-medium text-gray-700" htmlFor="user-id">
+                <label className="block text-sm font-medium text-gray-700" htmlFor="user-id">
                 User ID
-              </label>
-              <input
+                </label>
+                <input
                 className="mt-1 block w-full px-3 py-2 bg-gray-200 text-gray-700 rounded-md"
                 id="user-id"
-                type="text"
+                type="number"
                 value={userId}
-                onChange={(e) => setUserId(e.target.value)}
+                onChange={handleNumericInputChange(setUserId, 10)}
                 placeholder="User ID"
-              />
+                />
             </div>
 
             {/* First and Last Name */}
@@ -131,7 +139,7 @@ export default function EditProfile() {
                     className="flex-1 block w-full px-3 py-2 bg-gray-200 text-gray-700 rounded-none rounded-r-md"
                     id="phone-number"
                     value={phoneNumber}
-                    onChange={(e) => setPhoneNumber(e.target.value)}
+                    onChange={handleNumericInputChange(setPhoneNumber, 10)}
                     placeholder="5X XXX XXXX"
                   />
                 </div>
@@ -167,7 +175,7 @@ export default function EditProfile() {
                   id="weight"
                   type="text"
                   value={weight}
-                  onChange={(e) => setWeight(e.target.value)}
+                  onChange={handleNumericInputChange(setWeight, 3)}
                   placeholder="Weight"
                 />
               </div>
