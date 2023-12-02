1d4674060a060291a4300c528f2d52d4d73a6b80
Abdullah Ali <abdullaham1422@gmail.com>
2023-12-02 16:46:54 +0300

    DO some changes in the nav

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -57,8 +57,8 @@ export default function Nav() {
   return (
     <nav className="fixed top-0 left-0 z-[1000] bg-[#f7f7f7] flex items-center justify-between w-full px-16 pt-4 pb-4 font-medium max-md:px-8 md:ml-0 lg:ml-0">
       <div className="flex items-center gap-16">
-        <h1 className="text-2xl font-bold tracking-wider text-[#3e60a3] transition duration-300 ease-in-out delay-150 font-nunito hover:-translate-y-1 hover:scale-110">
-          <a href="/">InspireHub</a>
+        <h1 className="text-2xl font-bold tracking-wider text-[red] transition duration-300 ease-in-out delay-150 font-nunito hover:-translate-y-1 hover:scale-110">
+          <a href="/">Blood Donation System</a>
         </h1>
         <motion.div
           animate={{ opacity: 1, y: 0 }}
