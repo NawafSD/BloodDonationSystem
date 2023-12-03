523508981f1046e85f27a4ba5eabba56544dfcd0
Abdullah Ali <abdullaham1422@gmail.com>
2023-12-03 08:13:21 +0300

    Create some changes in the nav

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -27,7 +27,6 @@ const itemMotionDesktop = {
 
 const navLinks = [
   { name: "Home", href: "MainPage", id: 1 },
-  { name: "AI", href: "AIPage", id: 2 },
   { name: "About us", href: "AboutUsPage", id: 3 },
 ];
 
@@ -57,8 +56,8 @@ export default function Nav() {
   return (
     <nav className="fixed top-0 left-0 z-[1000] bg-[#f7f7f7] flex items-center justify-between w-full px-16 pt-4 pb-4 font-medium max-md:px-8 md:ml-0 lg:ml-0">
       <div className="flex items-center gap-16">
-        <h1 className="text-2xl font-bold tracking-wider text-[red] transition duration-300 ease-in-out delay-150 font-nunito hover:-translate-y-1 hover:scale-110">
-          <a href="/">Blood Donation System</a>
+        <h1 className="text-2xl font-bold tracking-wider text-red-700 transition duration-300 ease-in-out delay-150 font-nunito hover:-translate-y-1 hover:scale-110">
+          <a href="/">DonorHub</a>
         </h1>
         <motion.div
           animate={{ opacity: 1, y: 0 }}
@@ -79,24 +78,25 @@ export default function Nav() {
           className="fixed top-0 left-0 z-40 flex flex-col items-center justify-center w-full h-screen gap-24 text-2xl font-bold text-center bg-[#f7f7f7] "
         >
           <NavLinks
-            className="flex flex-col gap-24 text-lg text-black max-md:gap-12"
+            className="flex flex-col gap-24 text-lg text-[#121212] max-md:gap-12"
             isMobile={true}
           />
           <motion.div
             animate={{ opacity: 1, x: 0 }}
-            initial={{ opacity: 0, x: -75 }}
-            transition={{ delay: 0.9 }}
+            initial={{ opacity: 0, x: -55 }}
+            transition={{ delay: 0.7 }}
             className="flex flex-col w-64 gap-4"
           >
             <motion.a
-              className="w-full py-1 text-white  bg-[#5f7fbf] roundedtransition-all duration-700 hover:bg-[#3e60a3] focus:outline-none shadow-md hover:shadow-xl
+              className="w-full py-1 text-white  bg-[#292828] rounded transition-all duration-700 hover:bg-black focus:outline-none shadow-md hover:shadow-xl
               disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
               href="SignInPage"
             >
               Sign in
             </motion.a>
             <motion.a
-              className="w-full h-10 tracking-wide text-indigo-500 transition ease-in-out border-2 border-indigo-500 rounded hover:bg-indigo-500 hover:text-white active:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-300 duration-750"
+              className="w-full h-10 tracking-wide text-[#121212] transition-all duration-700 border-2 border-[#121212] rounded hover:bg-[#121212] hover:text-white ocus:outline-none shadow-md hover:shadow-xl
+              disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none "
               href="SignUpPage"
             >
               Sign up
@@ -113,8 +113,8 @@ export default function Nav() {
       >
         <a
           href="SignUpPage"
-          className="middle none center rounded-lg bg-[#5f7fbf] border-2 border-[#5f7fbf]  py-2 px-4.5 lg:py-1.5 lg:px-3.5 text-md font-bold font-nunito  text-white 
-           roundedtransition-all duration-700 hover:bg-[#3e60a3] focus:outline-none shadow-md hover:shadow-xl
+          className="middle none center rounded-lg bg-[#292828] border-2 border-[#292828]  py-2 px-4.5 lg:py-1.5 lg:px-3.5 text-md font-bold font-nunito  text-white 
+           roundedtransition-all duration-700 hover:bg-black focus:outline-none shadow-md hover:shadow-xl
            disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none cursor-pointer"
           data-ripple-light="true"
         >
@@ -122,8 +122,8 @@ export default function Nav() {
         </a>
         <a
           href="SignInPage"
-          className="middle none center rounded-lg border-2 border-[#5f7fbf] py-2 px-5 lg:py-1.5 lg:px-4 text-md font-bold font-nunito cursor-pointer  text-[#5f7fbf]
-           transition-all hover:opacity-75 focus:ring focus:ring-indigo-200 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          className="middle none center rounded-lg border-2 border-[#121212] py-2 px-5 lg:py-1.5 lg:px-4 text-md font-bold font-nunito cursor-pointer  text-[#121212]
+           transition-all hover:opacity-75 focus:ring focus:ring-[##292828] active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
           data-ripple-dark="true"
         >
           Sign in
@@ -141,12 +141,12 @@ export default function Nav() {
       >
         <motion.span
           animate={{ rotateZ: toggled ? 45 : 0, y: toggled ? 8 : 0 }}
-          className="line-1 block h-0.5 w-8 bg-black"
+          className="line-1 block h-0.5 w-8 bg-[#121212]"
         ></motion.span>
 
         <motion.span
           animate={{ width: toggled ? 0 : 24 }}
-          className="line-2 block h-0.5 w-6 bg-black"
+          className="line-2 block h-0.5 w-6 bg-[#121212]"
         ></motion.span>
         <motion.span
           animate={{
@@ -154,7 +154,7 @@ export default function Nav() {
             y: toggled ? -8 : 0,
             width: toggled ? 32 : 24,
           }}
-          className="line-3 block h-0.5 w-4 bg-black"
+          className="line-3 block h-0.5 w-4 bg-[#121212]"
         ></motion.span>
       </motion.div>
     </nav>
