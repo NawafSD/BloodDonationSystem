97e0bc5221f66191c00f43dfbf96f90a50a04357
Abdullah Ali <abdullaham1422@gmail.com>
2023-12-04 22:48:51 +0300

    Create a users edit page

---
+++ src/components/UsersEdit.tsx
@@ -0,0 +1,12 @@
+export default function UsersEdit() {
+
+    return (
+        <div className="bg-[#f7f7f7] pt-16 flex flex-col items-center min-h-screen font-roboto">
+          <div className="w-full max-w-4xl bg-white rounded-lg shadow-lg overflow-hidden flex flex-col items-center p-8 space-y-8">
+    
+            {/* Title */}
+            <h1 className="text-2xl font-semibold text-gray-900">Users</h1>
+            </div>
+        </div>
+    )
+    }
