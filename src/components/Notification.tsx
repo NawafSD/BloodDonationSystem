f1a13a9ce566129bf83b1c4fcbc85680d819cefb
Abdullah Ali <abdullaham1422@gmail.com>
2023-12-07 14:46:11 +0300

    Edit something in Notfication page

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { faCheck, faDollarSign } from '@fortawesome/free-solid-svg-icons';
 
-const NotificationItem = ({ notification, onAccept }) => {
-  const buttonClass = notification.isProcessing
-    ? "px-4 py-2 bg-blue-300 text-white rounded opacity-50 cursor-not-allowed" // Dim the button if processing
-    : "px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition-colors duration-300";
+const NotificationItem = ({ notification, onAccept, onPay }) => {
+  const buttonClass = "px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition-colors duration-300";
+  const payButtonClass = "px-7 py-2 bg-black text-white rounded hover:bg-gray-700 transition-colors duration-300";
 
   return (
+    
     <tr className="border-b">
       <td className="px-5 py-3 text-center">{notification.type}</td>
       <td className="px-5 py-3 text-center">{notification.from}</td>
@@ -15,17 +15,25 @@ const NotificationItem = ({ notification, onAccept }) => {
       <td className="px-5 py-3">
         <div className="flex justify-center items-center" style={{ height: '50px' }}>
           {notification.isCompleted ? (
-            <p>{notification.completedMessage}</p>
-          ) : notification.isProcessing ? (
-            <FontAwesomeIcon icon={faCheck} className="text-green-500" />
+            <FontAwesomeIcon icon={faCheck} className="text-green-500 m-11" />
           ) : (
-            <button
-              className={buttonClass}
-              onClick={() => onAccept(notification)}
-              disabled={notification.isProcessing}
-            >
-              Accept
-            </button>
+            notification.type === 'Request for Donate' ? (
+              <button
+                className={payButtonClass}
+                onClick={() => onPay(notification)}
+              >
+                <FontAwesomeIcon icon={faDollarSign} className="mr-2" />
+                Pay
+              </button>
+            ) : (
+              <button
+                className={buttonClass}
+                onClick={() => onAccept(notification)}
+              >
+                <FontAwesomeIcon icon={faCheck} className="mr-2" />
+                Accept
+              </button>
+            )
           )}
         </div>
       </td>
@@ -40,49 +48,35 @@ export default function Notification() {
       type: 'Request for Recipient',
       from: 'Hassan Alabdulal',
       date: '24/05/2023',
-      isCompleted: false,
-      isProcessing: false,
+      isCompleted: false
     },
     {
       id: 2,
       type: 'Request for Donate',
       from: 'Abdullah Al Matawah',
-      date: '23/05/2023',
-      isCompleted: false,
-      isProcessing: false,
+      date: '26/05/2023',
+      isCompleted: false
     },
-    // ... more notifications
   ]);
 
-  const handleAccept = (acceptedNotification) => {
-    // Update the notification to show the checkmark temporarily
+  const handleAccept = (notification) => {
     setNotifications((currentNotifications) =>
-      currentNotifications.map((notification) =>
-        notification.id === acceptedNotification.id
-          ? { ...notification, isProcessing: true }
-          : notification
+      currentNotifications.map((notif) =>
+        notif.id === notification.id
+          ? { ...notif, isCompleted: true }
+          : notif
       )
     );
+  };
 
-    // Simulate a 3-second processing delay
-    setTimeout(() => {
-      setNotifications((currentNotifications) =>
-        currentNotifications.map((notification) => {
-          if (notification.id === acceptedNotification.id) {
-            return {
-              ...notification,
-              isCompleted: true,
-              isProcessing: false,
-              completedMessage:
-                acceptedNotification.type === 'Request for Recipient'
-                  ? `The process of donating blood to ${notification.from} was completed successfully.`
-                  : `The process of receiving blood from ${notification.from} was completed successfully.`,
-            };
-          }
-          return notification;
-        })
-      );
-    }, 3000);
+  const handlePay = (notification) => {
+    setNotifications((currentNotifications) =>
+      currentNotifications.map((notif) =>
+        notif.id === notification.id
+          ? { ...notif, isCompleted: true }
+          : notif
+      )
+    );
   };
 
   return (
@@ -90,9 +84,14 @@ export default function Notification() {
       <div className="w-full max-w-4xl bg-white rounded-lg shadow-lg overflow-hidden flex flex-col items-center p-8 space-y-8">
         <h1 className="text-2xl font-semibold text-center text-gray-900">Notification Center</h1>
 
-        <div className="flex justify-center w-full">
-          <img src="src/assets/notification-bell.gif" alt="Notification Center" className="w-1/2 h-1/2 max-xl:w-80 max-xl:h-80 max-lg:mt-24" />
-        </div>
+                {/* Image */}
+                <div className="flex justify-center w-full">
+                  <img 
+                    src="src/assets/notification-bell.gif" 
+                    alt="Notification Center" 
+                    className="w-1/2 h-1/2 max-xl:w-80 max-xl:h-80 max-lg:mt-24" 
+                  />
+                </div>
 
         <table className="min-w-full">
           <thead>
@@ -100,7 +99,7 @@ export default function Notification() {
               <th className="px-5 py-3 text-center">Type</th>
               <th className="px-5 py-3 text-center">From</th>
               <th className="px-5 py-3 text-center">Date</th>
-              <th className="px-5 py-3 text-center width[150px]">Action</th>
+              <th className="px-5 py-3 text-center">Action</th>
             </tr>
           </thead>
           <tbody>
@@ -109,6 +108,7 @@ export default function Notification() {
                 key={notification.id}
                 notification={notification}
                 onAccept={handleAccept}
+                onPay={handlePay}
               />
             ))}
           </tbody>
