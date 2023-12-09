0c761670e940e41ac68cb3cbe7552c0dd158dcb5
Abdullah Ali <abdullaham1422@gmail.com>
2023-12-10 02:09:25 +0300

    Create the menu profile

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
+import ProfileMenu from "../components/UI/ProfileMenu";
 
 const navMotion = {
   visible: {
@@ -127,7 +128,10 @@ export default function Nav() {
         >
           Sign in
         </a>
+
+      <ProfileMenu toggled={toggled} />
       </motion.div>
+
 
       {/* Hamburger Toggle */}
       <motion.div
