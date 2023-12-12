674a6bb750284b60e3fcd2761aed518dbadde080
Hassan-Alsharyufi <h.alsharyoufi@gmail.com>
2023-12-13 00:53:55 +0300

    add functions to extrat the data from table but found problem with the ui that prevent from showing them

--- src/components/CollectionDriveReport.tsx
+++ src/components/CollectionDriveReport.tsx
@@ -1,6 +1,29 @@
 import TableWithStripedRows from "./UI/TableWithStripedRowsProps";
+import React, { useEffect, useState } from 'react';
+import { supabase } from '../../supabaseClient';
+
 
 export default function CollectionDriveReport() {
+  const [driveData , setDriveData] = useState<null | any[]>(null);
+  const [userError , setUserError] = useState<null | String>(null);
+
+useEffect(() => {
+  const getdrives =async () => {
+      const {data,error} = await supabase
+      .from('blooddrive')
+      .select()
+
+      if(data){
+          setDriveData(data)
+          setUserError(null)
+      }
+      if(error){
+          setDriveData(null)
+          setUserError("can't get data")
+      }
+      getdrives()
+  }
+},[])
   return (
     <main className="bg-[#f7f7f7] min-h-screen w-full flex flex-col items-center justify-center">
       <div className="mb-24">
@@ -9,9 +32,10 @@ export default function CollectionDriveReport() {
         </h1>
       </div>
       <div className="w-2/3">
-        <TableWithStripedRows
+      {userError && (<p>{userError}</p>)}
+          <TableWithStripedRows 
           headers={["Collection Drive", "Total Blood (Liters)"]}
-          rows={[
+          rows={[ 
             { type: "City Hall Blood Drive", bloodAmount: "75.0" },
             {
               type: "Healthcare Heroes Donation Campaign",
@@ -23,9 +47,11 @@ export default function CollectionDriveReport() {
             { type: "Athletes for Life Blood Drive", bloodAmount: "53.2" },
             { type: "Hometown Heroes Blood Drive", bloodAmount: "48.1" },
             { type: "Holiday Season Blood Drive", bloodAmount: "67.9" },
-          ]}
+         ]}
         />
+      
       </div>
     </main>
   );
 }
+
